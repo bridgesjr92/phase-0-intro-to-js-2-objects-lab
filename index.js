a1179de7831d4407feaa2e0d1691ee3f0e08567c
darrylbridges <bridgesjr92@gmail.com>
2024-04-06 06:25:47 -0700

    added

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+
+let employee = {}
+
+
+
+function updateEmployeeWithKeyAndValue(employee, name , streetAddress){
+    let newEmployee = {...employee}
+    newEmployee[name] = streetAddress
+        return newEmployee
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name , streetAddress){
+    // let newEmployee = {...employee}
+    employee[name] = streetAddress
+        return employee
+}
+
+
+function deleteFromEmployeeByKey(employee, name){
+    let newEmployee = {...employee}
+    delete newEmployee[name]
+        return newEmployee
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    // let newEmployee = {...employee}
+    delete employee[name]
+        return employee
+}
+
+
+// function updateEmployeeWithKeyAndValue(employee, key, value){}
+
+
+// function updateEmployeeWithKeyAndValue(employee, key, value){}
+
+
+// function updateEmployeeWithKeyAndValue(employee, key, value){}
